78c33a1aa8379797ff2b75f72941acaba5d8f39d
Andre Lino <andre.cesar97@hotmail.com>
2023-07-19 16:48:48 -0300

    ajustado a umidade

--- src/components/Temp.tsx
+++ src/components/Temp.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 
 export function Temp() {
   const [temperature, setTemperature] = useState(30)
+  const [umidity, setUmidity] = useState(70)
   const [isDaytime, setIsDaytime] = useState(false)
 
   useEffect(() => {
@@ -24,11 +25,11 @@ export function Temp() {
     <main className={`flex flex-col items-center justify-center h-screen ${isDaytime ? 'bg-blu-50' : 'bg-blue-950'}`}>
       <p className={`text-xl font-bold ${isDaytime ? 'text-black' : 'text-white'}`}>Temperatura e umidade interna:</p>
       <div className={`flex items-center justify-center space-x-2 ${isDaytime ? 'text-black' : 'text-white'}`}>
-                   {isDaytime ?  <Sun size={56} color="yellow" /> : <Moon size={56} color="white" />}        
+        {isDaytime ?  <Sun size={56} color="yellow" /> : <Moon size={56} color="white" />}        
         <text className={`text-6xl font-bold ${isDaytime ? 'text-black' : 'text-white'}`}>{temperature}°C</text>
       </div>
       <div className="flex items-center justify-end">
-        <text className={`text-lg ${isDaytime ? 'text-black' : 'text-white'}`}>Umidade: 70%</text>
+        <text className={`text-lg ${isDaytime ? 'text-black' : 'text-white'}`}>Umidade: {umidity}%</text>
       </div>
     </main>
   )
